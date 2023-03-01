7692b63c5958f4c73be82c171d490706f438a572
ZeljanaGrujic <zg20180045@gmail.com>
2023-03-01 19:29:55 +0100

    Add event operation

---
+++ src/Operations.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Operations {
+
+
+    List<Event> events= new ArrayList<>();
+
+    public void addEvent(Event e){
+        events.add(e);
+    }
+}
